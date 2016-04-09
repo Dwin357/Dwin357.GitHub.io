540ace2342928157d80967d24ce3d3b320923178
dwin357 <e.r.steinmetz@gmail.com>
2016-04-09 16:40:22 -0500

    reworked file structure in anticipation of adding jasmine

---
+++ js/spec/GameSpec.js
@@ -0,0 +1,40 @@
+describe("Game", function() {
+  var game;
+  var result;
+  var valueTwoOrFour;
+  
+  beforeEach(function() {
+    game = new Game();
+  });
+
+  describe ("initialize", function() {
+
+    beforeEach(function() {
+      result = [];
+      for(var i=0; i < game.state.length; i++){
+        if (game.state[i] != null){
+          result[result.length] = game.state[i];
+        }
+      }
+
+      valueTwoOrFour = function(input){
+        return (input == 4) || (input == 2)
+      };
+
+    });
+
+    it ("has a game 16 length game state", function(){
+      expect(game.state.length).toEqual(16);
+    });
+    it ("populates two random positions", function(){
+      expect(result.length).toEqual(2);
+    });
+    it ("populates first position with a 2 or 4", function(){
+      expect(valueTwoOrFour(result[0])).toBe(true);
+    });
+    it ("populates second position with a 2 or 4", function(){
+      expect(valueTwoOrFour(result[1])).toBe(true);
+    });
+  });
+
+});
